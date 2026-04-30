1455e674bdd66ae47aaa55f89bb9bffb8cfe30a5
SuryaGanesh284 <surya285888@gmail.com>
2026-04-30 22:50:21 +0530

    Robust database config with sqlite fallback

--- constitution_platform/settings.py
+++ constitution_platform/settings.py
@@ -90,13 +90,22 @@ WSGI_APPLICATION = 'constitution_platform.wsgi.application'
 # Environment variables:
 #   DATABASE_URL
 #
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}   
+db_url = os.environ.get('DATABASE_URL')
+if db_url:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            db_url,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }   
 
 
 # Password validation
